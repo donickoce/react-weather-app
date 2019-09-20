59e4e3713ae79daf81026329bf0c70854bcbbafd
Lazar <layarkostic94@gmail.com>
2019-09-20 13:08:44 +0200

    Add files via upload

---
+++ src/components/CitySelector.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Row, Col, FormControl, Button } from 'react-bootstrap';
+
+const CitySelector = ({ onSelectButtonClick }) => {
+
+    const [city, setCity] = useState(null);
+
+    return (
+        <>
+            <Row>
+                <Col>
+                    <h1>Hello, select your city!</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={4}>
+                    <FormControl
+                        placeholder="Enter city"
+                        onChange={e => setCity(e.target.value)}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button onClick={() => onSelectButtonClick(city)}>Check Weather</Button>
+                </Col>
+            </Row>
+        </>
+    )
+
+};
+
+export default CitySelector;
